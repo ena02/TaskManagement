1bbed223cedfcbb128c91cf4a21e3e9d1eda6b86
ena <erkembekov.nurzhan@mail.ru>
2024-07-16 10:19:12 +0500

    create db context

---
+++ TaskManagement/Data/ApplicationDbContext .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+
+namespace TaskManagement.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
